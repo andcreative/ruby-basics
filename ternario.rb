ee1867bb6ab540179acbe01f0db0e496c7072575
andre <andre.decarvalho1@gmail.com>
2022-08-05 16:25:17 -0300

    add ternarios

--- ternario.rb
+++ ternario.rb
@@ -1,7 +1,17 @@
 nome = "Andre"
+n = 4
 
-if nome.eql?("Andre")
-    puts "Web developer"
-else
-    puts "Visitante"
-end
+# if nome.eql?("Andre")
+#     puts "Web developer"
+# else
+#     puts "Visitante"
+# end
+
+# condição ? verdadeiro : falso
+
+puts nome.eql?("Andre") ? "Web Developer" : "Visitante"
+resultado = nome.eql?("Andre") ? "Web Developer - resultado" : "Visitante - resultado"
+puts resultado
+
+soma = n.eql?(5) ? n + 10 : n - 1
+puts soma
